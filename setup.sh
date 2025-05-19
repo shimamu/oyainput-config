2f661b1e6069616ec3f4754b4803d8ff1bd491cf
shimamu <cooklecurry@gmail.com>
2025-05-20 00:47:41 +0900

    feat: Enable application autostart on GNOME

--- setup.sh
+++ setup.sh
@@ -2,4 +2,12 @@
 
 # Set up script for Linux.
 
+# Create a symbolic link to .oyainputconf in the home directory
 ln -sf -T `pwd`/.oyainputconf ~/.oyainputconf
+
+# Ensure the autostart directory exists
+mkdir -p ~/.config/autostart
+
+# Link output.desktop to the autostart directory as oyainput.desktop
+ln -sf -T `pwd`/oyainput.desktop ~/.config/autostart/oyainput.desktop
+
